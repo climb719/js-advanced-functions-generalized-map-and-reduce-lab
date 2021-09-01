72b679b152aacc51cff31d17b9e4a70fa954f45d
climb719 <mary.oldweiler@gmail.com>
2021-09-01 09:43:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(sourceArray, callback) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        const element = sourceArray[i]
+     newArray.push(callback(element)) 
+    }
+    return newArray
+  }
+
+
+
+
+  function reduce(sourceArray, callback, startingPoint) {
+    if (startingPoint) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      
+    }
+    }
+  }
